46bd76586b68fffedc9c90714d556c7a06faffeb
ZarembaBar <ZarembaBar@github.com>
2023-07-21 12:25:02 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from pyautogui import position, moveTo
+from random import randint
+from screeninfo import get_monitors
+from time import sleep
+
+for i in get_monitors():
+    screen_width = i.width
+    screen_height = i.height
+
+for j in range(10):
+    cursor_value_x = randint(1, screen_width)
+    cursor_value_y = randint(1, screen_height)
+    actual_cursor_position = position()
+
+    if cursor_value_x != actual_cursor_position[0] and cursor_value_y != actual_cursor_position[1]:
+        moveTo(cursor_value_x, cursor_value_y, 0.5)
+        sleep(2)
