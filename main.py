0b88d5131bf3df2e0f9da49faa5ab9c7bc033f3b
ZarembaBar <ZarembaBar@github.com>
2023-07-21 12:57:23 +0200

    add terminal line input

--- main.py
+++ main.py
@@ -3,16 +3,30 @@ from random import randint
 from screeninfo import get_monitors
 from time import sleep
 
+print("How many minutes would you like to keep your computer active")
+while True:
+    try:
+        user_input = int(input())
+        if user_input:
+            break
+    except ValueError:
+        print("Program takes only full number values\n"
+              "try again: ")
+
+"Time counted for later loop usage in every 2 seconds rounds"
+counted_time = (user_input * 60) / 2
+
 for i in get_monitors():
     """Get only X and Y values from Monitor Class"""
     screen_width = i.width
     screen_height = i.height
 
-for j in range(10):
+for j in range(int(counted_time)):
     """Choose random X and Y positions and actual cursor position to use it in later comparison"""
     cursor_value_x = randint(1, screen_width)
     cursor_value_y = randint(1, screen_height)
     actual_cursor_position = position()
+    print(j)
 
     if cursor_value_x != actual_cursor_position[0] and cursor_value_y != actual_cursor_position[1]:
         moveTo(cursor_value_x, cursor_value_y, 0.5)
