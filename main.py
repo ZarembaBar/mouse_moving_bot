77c42b1b28bf2dbec759e1107ce69339bc00b952
ZarembaBar <ZarembaBar@github.com>
2023-07-21 12:34:52 +0200

    Add comments

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from screeninfo import get_monitors
 from time import sleep
 
 for i in get_monitors():
+    """Get only X and Y values from Monitor Class"""
     screen_width = i.width
     screen_height = i.height
 
 for j in range(10):
+    """Choose random X and Y positions and actual cursor position to use it in later comparison"""
     cursor_value_x = randint(1, screen_width)
     cursor_value_y = randint(1, screen_height)
     actual_cursor_position = position()
